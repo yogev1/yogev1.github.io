c097e6d0458ca4305e5ea6e63e83e41711a95a2b
Yogev <finegoz@gmail.com>
2017-11-09 00:26:46 -0800

    update

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,4 @@
 class Contact < MailForm::Base
-  attribute :name,      :validate => true
-  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-  attribute :message
-
   # Declare the e-mail headers. It accepts anything the mail method
   # in ActionMailer accepts.
   def headers
