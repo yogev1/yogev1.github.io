d443ac7ef339fb84491769ddb96da013856288ce
Yogev <finegoz@gmail.com>
2017-11-13 11:01:53 -0800

    sendgrid credentials

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,7 +4,7 @@ class ContactsController < ApplicationController
   end
 
   def create
-    @contact = Contact.new(params[:contact])
+    @contact = Contact.new(contact_params)
     @contact.request = request
     if @contact.deliver
       flash.now[:notice] = 'Thank you for your message. I will contact you soon!' 
